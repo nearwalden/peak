5cd67e5b48f7e3a86035f2ee7dcc6b1ed313283f
David Douglas <nearwalden@gmail.com>
2021-01-04 02:10:50 +0000

    starting locations

--- locations.py
+++ locations.py
@@ -1,10 +1,25 @@
 # normalize locations
 
-BASE_PATH = "/Volumes/data/peak/population/"
-BGMF_PATH = "gatesfoundation/IHME_POP_2017_2100_POP_BOTH_SEX_ALL_AGE_Y2020M05D01.csv"
-UN_PATH = "un-wpp2019/WPP2019_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES_medium.csv"
-WITT_PATH = "wittgenstein-center/recode\ file.csv"
+from prefect import flow
+from atlas import data
+import pandas as p
 
-OUT_PATH = "common/locations.csv"
+@flow
+def bmgf_locations ():
+	path = data.path('bmgf_population_2019_all', 'bmgf_population_2019')
+	df = p.read_csv (path)
+	dfg = df.groupby('location')
+	return dfg['location_id'].first()
+	
+@flow 
+def witt_locations ():
+	
+@flow
+def un_locations ():
+	
+
+
+
+
 
 
